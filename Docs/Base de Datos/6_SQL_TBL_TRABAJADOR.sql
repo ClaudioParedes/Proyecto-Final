6ad4a0dfd24d8debcaaaf2d3f6ccc7ed08fbd450
claudio <claudioparedes1@gmail.com>
2022-06-18 00:04:36 -0400

    Correccion Sintaxis

--- Docs/Base de Datos/6_SQL_TBL_TRABAJADOR.sql
+++ Docs/Base de Datos/6_SQL_TBL_TRABAJADOR.sql
@@ -1,10 +1,13 @@
 CREATE TABLE trabajador (
 	id_trabajador VARCHAR(50),
+	id_contrato VARCHAR(50),
 	rut VARCHAR(13),
 	nombre VARCHAR(15),
 	apellido VARCHAR(15),
     fecha_nacimiento DATE,
     cargo VARCHAR(20),
     foto VARCHAR(100),
-	PRIMARY KEY(id_trabajador)
+	PRIMARY KEY(id_trabajador),
+	FOREIGN KEY (id_contrato) REFERENCES contrato(id_contrato)
+
 );

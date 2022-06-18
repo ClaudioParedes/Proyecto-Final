10dc2455b8481f057b532032720829dd8a208889
claudio <claudioparedes1@gmail.com>
2022-06-17 23:29:02 -0400

    SQL Tablas Agregadas

---
+++ Docs/Base de Datos/5_SQL_TBL_AREA.sql
@@ -0,0 +1,4 @@
+CREATE TABLE area (
+	id_area VARCHAR(50),
+	PRIMARY KEY(id_area),
+);
---
+++ Docs/Base de Datos/6_SQL_TBL_CONTRATO.sql
@@ -0,0 +1,8 @@
+CREATE TABLE contrato (
+	id_contrato VARCHAR(50),
+    empresa VARCHAR(15),
+    nro_contrato INT,
+    nro_contrato_interno INT,
+    fecha_ingreso DATE,
+	PRIMARY KEY(id_actividad),
+);
